1b4cb3f28a802665ce0a3f31a2523e35d2215b9d
andrereis26 <gato5andre26@gmail.com>
2023-03-05 04:16:44 +0000

    Added resets configs and hotkeys. Change channels' name TO-DO

--- bot.py
+++ bot.py
@@ -8,12 +8,12 @@ import asyncio                      # asyncio
 import keyboard                     # keyboard
 import configparser                 # config parser
 import json                         # json
-
+import atexit                       # atexit
 
 # global
+config = None           # config
 me = None               # discord's user obj
 lastNickname = None     # user's last nickname
-config = None           # config
 
 # bot stuff
 intents = discord.Intents.default()
@@ -35,6 +35,21 @@ def loadConfigFile():
     config.read(configFilePath)
 
 
+# Func that resets the nick/channel (it will reset based on your changeMode value on the config.ini file) to its default value
+def resetValues():
+    # check what mode the bot is on
+    if (json.loads(config.get("MAIN", "changeMode")) == "nick"):
+        # resets the user's nick
+        # loop through all servers and resets its nickname
+        loopChangeNickname(json.loads(config.get("NICK", "defaultNick")))
+        
+        print("Reset all your nicks!")
+    else:
+        # resets the channels' name
+        print("TO-DO")
+        print("Reset all your channels' name!")
+
+
 # Func to change the nickname
 # note: since i need to deal with async functions (the discord stuff) and this function (changeNickname) can't be async cuz the keyboard stuff isnt async,
 # i gotta create tasks instead to deal with it
@@ -42,7 +57,7 @@ def changeNickname(newNick):
 
     # calls the global var
     global lastNickname
-    print(me.nick)
+   
     # cheks if nicks are the same, if its true, that means the user pressed the same keybind, that is, will redo the nickname changes
     if (newNick != me.nick):
         # store current nickname
@@ -55,15 +70,7 @@ def changeNickname(newNick):
     if (json.loads(config.get("NICK", "changeOnAllServers")) == True):
         try:
             # loop through all servers and change its nickname
-            for guild in client.guilds:
-                # finds the user in the current guild
-                findMe = guild.get_member(me.id)
-
-                # changes its nickname
-                msg = asyncio.run_coroutine_threadsafe(
-                    findMe.edit(nick=newNick), client.loop)
-
-                # msg.result(timeout=15)
+            loopChangeNickname(newNick)
 
         except Exception as e:
             print(e)
@@ -83,6 +90,19 @@ def changeNickname(newNick):
             print("It wasn't possible to change your nickname. Make sure u executaded the command /assign before trying to change your nickname")
         else:
             print("Nick changed")
+
+
+# Func that loops throught all the servers and changes/resets the user's nick
+def loopChangeNickname(newNick):
+    # loop through all servers and change its nickname
+    for guild in client.guilds:
+        # finds the user in the current guild
+        findMe = guild.get_member(me.id)
+
+        # changes its nickname
+        msg = asyncio.run_coroutine_threadsafe(
+            findMe.edit(nick=newNick), client.loop)
+        # msg.result(timeout=15)
 
 
 # Func to change a channel's name
@@ -113,6 +133,16 @@ def createHotKeys():
                                 args=[json.loads(config.get("CHANNEL", "channelNames"))[i]])
             i += 1
 
+    # adds reset hotkey (if configed)
+    if (bool(json.loads(config.get("HOTKEYS", "reset")) and json.loads(config.get("HOTKEYS", "reset")).strip())):
+        keyboard.add_hotkey(json.loads(
+            config.get("HOTKEYS", "reset")), resetValues)
+
+    # adds end hotkey (if configed)
+    if (bool(json.loads(config.get("HOTKEYS", "end")) and json.loads(config.get("HOTKEYS", "reset")).strip())):
+        keyboard.add_hotkey(json.loads(
+            config.get("HOTKEYS", "reset")), resetValues)
+
     print("HotKeys created!")
 
 
@@ -126,7 +156,7 @@ def assignUser():
         # assign me
         me = client.get_guild(json.loads(config.get("NICK", "serversIds"))[0]
                               ).get_member(json.loads(config.get("MAIN", "myId")))
-        
+
     else:
         # assign me
         me = client.get_guild(json.loads(config.get("CHANNEL", "serverId"))).get_member(
@@ -143,6 +173,11 @@ async def on_ready():
 
     # assign user
     assignUser()
+
+    # checks if user wants its nick/channel (it will reset based on your changeMode value on the config.ini file) to its default value
+    if (json.loads(config.get("MAIN", "resetOnClose")) == True):
+        # registers a function to be executed when the program is terminating
+        atexit.register(resetValues)
 
     # create a thread for keyboard stuff
     keyboardThread = Thread(target=createHotKeys, args=())
