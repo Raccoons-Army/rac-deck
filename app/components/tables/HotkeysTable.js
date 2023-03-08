5f859e9ab16e94215d27d3b72c55b7253be08fb4
andrereis26 <gato5andre26@gmail.com>
2023-03-08 17:31:19 +0000

    Added UI for the change name bot config

---
+++ app/components/tables/HotkeysTable.js
@@ -0,0 +1,84 @@
+import {
+    faTrash
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from 'react';
+import { Table } from 'reactstrap';
+
+export default function HotkeysTable({ dataType }) {
+
+    const [tableData, setTableData] = useState([]);
+    const [columns, setColumns] = useState([])
+
+    // load table content
+    useEffect(() => {
+        console.log("Table content: " + dataType);
+        if (dataType == "Nick") {
+            //TODO
+            setColumns(["Server", "Nick"])
+        } else {
+            //TODO
+            setColumns(["Channel", "Name"])
+        }
+
+    }, [dataType])
+
+    // handle to delete hotkey
+    const handleDelete = (id) => {
+
+    }
+
+    return (
+        <>
+            <Table
+                bordered
+                responsive
+                size=""
+                striped
+            >
+                <thead>
+                    <tr>
+
+                        {columns?.map((c) => {
+                            return (
+                                <th key={c}>
+                                    {c}
+                                </th>
+
+                            )
+                        })}
+                        <th >
+                            Hotkey
+                        </th>
+                        <th>
+                            On/Off
+                        </th>
+                        <th>
+                            Actions
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td >
+                            Mark
+                        </td>
+                        <td>
+                            Otto
+                        </td>
+                        <td>
+                            @mdo
+                        </td>
+                        <td>
+                            @mdo
+                        </td>
+                        <td align='center' onClick={() => handleDelete(2)}>
+                            <FontAwesomeIcon color='red' icon={faTrash} className="mr-2" />
+                        </td>
+                    </tr>
+
+                </tbody>
+            </Table>
+        </>
+    )
+}
--- app/pages/changeNameBot.js
+++ app/pages/changeNameBot.js
@@ -1,11 +1,156 @@
-import React from 'react';
-import { Button } from 'reactstrap';
+import React, { useEffect, useState } from 'react';
+
+// components
+import { Form, Row, Col, FormGroup, Label, Input, Button, Table } from 'reactstrap';
+import HotkeysTable from '@/components/tables/HotkeysTable';
+import HotkeyInput from '@/components/others/HotkeyInput'
+
+// custom hooks
+import { useFormStringInput, useFormSwitchInput, useFormSelectBox } from '@/utility/customHooks'
 
 export default function ChangeNameBot() {
+
+    // main info
+    const botToken = useFormStringInput("test")
+    const changeOnStart = useFormSwitchInput(false);
+    const botMode = useFormSelectBox(["Nick", "Channel"], "Nick")
+
+    // nick info
+    const discordId = useFormStringInput("")
+    const changeOnAllServers = useFormSwitchInput(false);
+
+    // channel info
+    const serverId = useFormStringInput("")
+
+
     return (
         <>
-            <Button color="info">Hi!</Button>
-            
+            <h2>Main</h2>
+            <hr></hr>
+            <Form>
+                <Row>
+                    <Col md={3}>
+                        <FormGroup>
+                            <Label for="exampleEmail">
+                                Bot Token
+                            </Label>
+                            <Input
+                                id="botToken"
+                                name="botToken"
+                                placeholder="paste your bot token here"
+                                type="password"
+                                onChange={botToken.handleChange}
+                            />
+                        </FormGroup>
+                    </Col>
+                    <Col md={3}>
+                        <FormGroup>
+                            <Label for="botMode">
+                                Bot Mode
+                            </Label>
+                            <Input
+                                id="exampleSelect"
+                                name="select"
+                                type="select"
+                                onChange={botMode.handleChange}
+                            >
+                                {
+                                    botMode.options.map((x, i) => {
+                                        return (
+                                            <option key={"mode:" + x} value={x}>
+                                                {x}
+                                            </option>
+                                        )
+                                    })
+                                }
+                            </Input>
+                        </FormGroup>
+                    </Col>
+                    <Col md={3}>
+                        <Label for="resetHotkey">
+                            Reset Hotkey
+                        </Label>
+                   <HotkeyInput/>
+
+                    </Col>
+                    <Col md={3}>
+                        <Label for="changeOnStart">
+                            Change On Start
+                        </Label>
+                        <FormGroup switch>
+                            <Input id="changeOnStart"
+                                name="changeOnStart"
+                                type="switch" checked={changeOnStart.value}
+                                onChange={changeOnStart.handleChange} />
+
+                        </FormGroup>
+                    </Col>
+                </Row>
+                {botMode.value === "Nick" ?
+                    <>
+                        <Row>
+                            <Col md={6}>
+                                <FormGroup>
+                                    <Label for="userDiscordId">
+                                        Your Discord ID - DPS FZR CONECTAR COM DISCORD PRA N PRECISAR DISTO
+                                    </Label>
+                                    <Input
+                                        id="userDiscordId"
+                                        name="userDiscordId"
+                                        placeholder="paste your discord id here"
+                                        type="password"
+                                        onChange={discordId.handleChange}
+                                    />
+                                </FormGroup>
+                            </Col>
+                            <Col md={4}>
+                                <Label for="changeOnAllServers" >
+                                    Change On All Servers
+                                </Label>
+                                <FormGroup switch>
+                                    <Input id="changeOnAllServers"
+                                        name="changeOnAllServers"
+                                        type="switch" checked={changeOnAllServers.value}
+                                        onChange={changeOnAllServers.handleChange} />
+
+                                </FormGroup>
+                            </Col>
+                        </Row>
+                    </>
+                    : // else
+                    <>
+                        <Row>
+                            <FormGroup>
+                                <Label for="userDiscordId">
+                                    Server ID - DPS FZR CONECTAR COM DISCORD PRA FAZER UMA SELECT ANTES
+                                </Label>
+                                <Input
+                                    id="userDiscordId"
+                                    name="userDiscordId"
+                                    placeholder="paste your discord id here"
+                                    type="password"
+                                    onChange={serverId.handleChange}
+                                />
+                            </FormGroup>
+                        </Row>
+                    </>}
+                <Button type='button' color='success' >
+                    Save
+                </Button>
+
+                <br />
+                <br />
+            </Form>
+
+            <h2>Hotkeys</h2>
+            <hr></hr>
+            <Row>
+                {botMode.value ? (<HotkeysTable dataType={botMode.value} />) : (<>Select a mode</>)}
+
+            </Row>
+
+
+
         </>
     )
 }
